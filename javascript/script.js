671e026a5e81308efd0f17fe94068272d50edc91
nimzsamuel <ponmwasamuel2020@gmail.com>
2025-06-26 14:34:52 +0100

    update

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+var user = {
+    userName : "nimzsamuel",
+    password : "123456789",
+};
+
+var database = [user];
+
+var newsFeed = [
+    {
+        userName : 'jenny',
+        timeline : 'javascript is sweat'
+    },
+    {
+        userName: 'james',
+        timeline: 'welcome to the wrold of programming'
+    },
+    {
+        userName : 'favour',
+        timeline : 'you are a good upcoming programmer'
+    }
+] 
