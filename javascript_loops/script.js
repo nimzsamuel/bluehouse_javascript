7fd66157ee0809361566df1f0d7760ccbd9f05db
nimzsamuel <ponmwasamuel2020@gmail.com>
2025-07-08 11:53:29 +0100

    update

---
+++ javascript_loops/script.js
@@ -0,0 +1,20 @@
+//write a for loop that print out all even numbers between 1 to 10
+for (var i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+};
+
+// write a while loop that counts from 10 downwards
+var i = 10;
+while (i > 0){
+    console.log (i);
+    i--
+};
+
+//write a do-while loop that ponit the number 5 only ince even when the condition is false
+var i = 5;
+do {
+    console.log (i);
+    i++
+} while(i < 4)
