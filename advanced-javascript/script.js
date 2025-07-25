516efb3929da039ebf12c088de47e29a20f6550e
nimzsamuel <ponmwasamuel2020@gmail.com>
2025-07-25 10:07:56 +0100

    update

--- advanced-javascript/script.js
+++ advanced-javascript/script.js
@@ -221,3 +221,48 @@ function whereAmI(username, location) {
 
 //answer
 const whereAmI = (username, location) => (username && location) ? "I am not lost" : "I am totally lost!";
+
+// 8.1 exercise
+//Solve these problems:
+
+//#1 Create a one line function that adds adds two parameters
+//answer
+const add = (a, b) => a + b;
+
+//Closure: What does the last line return?
+const addTo = x => y => x + y;
+var addToTen = addTo(10);
+addToTen(3);
+
+//answer is 13
+
+//Currying: What does the last line return?
+const sum = (a, b) => a + b;
+const curriedSum = (a) => (b) => a + b;
+curriedSum(30)(1);
+
+//answer is 31
+
+
+
+//Currying: What does the last line return?
+const sum = (a, b) => a + b
+const curriedSum = (a) => (b) => a + b
+const add5 = curriedSum(5)
+add5(12)
+
+//answer is 17
+
+//Composing: What does the last line return?
+const compose = (f, g) => (a) => f(g(a));
+const add1 = (num) => num + 1;
+const add5 = (num) => num + 5;
+compose(add1, add5)(10)
+
+//answer is 16
+
+//What are the two elements of a pure function?
+//1. No Side Effects
+
+//2. Same Output for Same Input
+
